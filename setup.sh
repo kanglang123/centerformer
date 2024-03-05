c519c72c5941303329a61bb9e7608567daf0f372
kanglang123 <yk_wang@sjtu.edu.cn>
2024-03-06 00:32:06 +0800

    1

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@
 cd det3d/ops/iou3d_nms
 python setup.py build_ext --inplace
 
-cd ../.. && cd models/ops/
+cd ../.. && cd models/ops/h
 python setup.py build install
 # unit test (should see all checking is True)
 python test.py
